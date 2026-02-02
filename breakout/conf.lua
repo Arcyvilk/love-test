cf4f654f5c07a2284a85d8c9a794bdf163201c77
Arcyvilk <arcyvilk@pm.me>
2026-02-02 16:49:10 +0100

    refactor: 💡 reogranize files

--- breakout/conf.lua
+++
@@ -1,7 +0,0 @@
-local vars = require 'vars'
-
-love.conf = function(t)
-  t.console = true                    -- Enable the debug console for Windows.
-  t.window.width = vars.world_width   -- Game's screen width (number of pixels)
-  t.window.height = vars.world_height -- Game's screen height (number of pixels)
-end
