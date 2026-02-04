d3769607ff2810968018ce5df93ae56093947900
Arcyvilk <arcyvilk@pm.me>
2026-02-04 14:52:13 +0100

    refactor: 💡 enable player movement

--- balls/main.lua
+++ balls/main.lua
@@ -1,6 +1,7 @@
 local world           = require 'world'
 local input           = require 'input'
 local entities        = require 'entities.entities'
+local player          = require 'entities.player'
 
 local draw_boundaries = function()
   for _, entity in ipairs(entities.boundaries) do
@@ -23,23 +24,31 @@ local draw_spawners   = function()
 end
 
 love.draw             = function()
+  player:draw()
   draw_boundaries()
   draw_spawners()
 end
 
+love.update           = function(dt)
+  player:update()
 
-love.update = function(dt)
   for _, spawner in ipairs(entities.spawners) do
     if spawner.spawn then
-      spawner:spawn()
+      spawner:spawn(dt)
     end
   end
 
   world:update(dt)
 end
 
-love.keypressed = function(pressed_key)
-  if input[pressed_key] then
-    input[pressed_key]()
+love.keypressed       = function(pressed_key)
+  if input[pressed_key] and input[pressed_key].press then
+    input[pressed_key]:press()
+  end
+end
+
+love.keyreleased      = function(released_key)
+  if input[released_key] and input[released_key].release then
+    input[released_key]:release()
   end
 end
--- balls/vars.lua
+++ balls/vars.lua
@@ -6,9 +6,17 @@ local vars = {
   world_height = 600,
   boundary_width = 2,
   boundary_style = 'fill',
+  boundary_offset_top = 50,
 
   ball_radius = 10,
-  spawner_offset = 25
+  ball_speed = 1,
+  ball_spawn_delay = 1,
+  spawner_offset_x = 50,
+  spawner_offset_y = 100,
+
+  player_radius = 10,
+  player_health = 5,
+  player_speed = 5
 }
 
 return vars
