8ba893c2b259856fda6e9e66bd346207a80c18ce
Arcyvilk <arcyvilk@pm.me>
2026-02-04 15:00:41 +0100

    refactor: 💡 fix player boundaries

--- balls/entities/player.lua
+++ balls/entities/player.lua
@@ -11,37 +11,38 @@ entity.shape   = love.physics.newCircleShape(vars.player_radius)
 entity.fixture = love.physics.newFixture(entity.body, entity.shape)
 entity.fixture:setUserData(entity)
 
-entity.draw    = function(self)
-  local x_self, y_self = self.body:getWorldCenter()
-  love.graphics.circle('fill', x_self, y_self, self.shape:getRadius())
-end
-
 entity.update  = function(self)
   local x_self, y_self = self.body:getPosition()
 
   if input.left.active then
-    local boundary_left = math.max(x_self - vars.player_speed, vars.player_radius / 2)
+    local boundary_left = math.max(x_self - vars.player_speed, vars.player_radius)
     self.body:setPosition(boundary_left, y_self)
   end
 
   if input.right.active then
-    local boundary_right = math.min(x_self + vars.player_speed, vars.world_width - vars.player_radius / 2)
+    local boundary_right = math.min(x_self + vars.player_speed, vars.world_width - vars.player_radius)
     self.body:setPosition(boundary_right, y_self)
   end
 
   if input.up.active then
-    local boundary_top = math.max(vars.player_radius / 2, y_self - vars.player_speed)
+    local boundary_top = math.max(vars.boundary_offset_top + vars.player_radius, y_self - vars.player_speed)
     self.body:setPosition(x_self, boundary_top)
   end
 
   if input.down.active then
-    local boundary_down = math.min(vars.world_height - vars.player_radius / 2, y_self + vars.player_speed)
+    local boundary_down = math.min(vars.world_height - vars.player_radius, y_self + vars.player_speed)
     self.body:setPosition(x_self, boundary_down)
   end
 end
 
 entity.collide = function(self)
   self.health = self.health - 1
+end
+
+
+entity.draw = function(self)
+  local x_self, y_self = self.body:getWorldCenter()
+  love.graphics.circle('fill', x_self, y_self, self.shape:getRadius())
 end
 
 return entity
