4d5fa95a40b0013b7b7450dcdd57c27ccb2916df
Arcyvilk <arcyvilk@pm.me>
2026-02-04 15:24:29 +0100

    refactor: 💡 define player entity

---
+++ dodge/entities/player.lua
@@ -0,0 +1,41 @@
+local world    = require 'world'
+local vars     = require 'vars'
+local input    = require 'input'
+
+local entity   = {}
+
+entity.body    = love.physics.newBody(world, vars.world_width / 2, vars.world_height / 2, 'kinematic')
+entity.shape   = love.physics.newCircleShape(vars.player_radius)
+entity.fixture = love.physics.newFixture(entity.body, entity.shape)
+entity.fixture:setUserData(entity)
+
+entity.update = function(self)
+  local x_self, y_self = self.body:getPosition()
+
+  if input.left.active then
+    local boundary_left = math.max(x_self - vars.player_speed, vars.player_radius)
+    self.body:setPosition(boundary_left, y_self)
+  end
+
+  if input.right.active then
+    local boundary_right = math.min(x_self + vars.player_speed, vars.world_width - vars.player_radius)
+    self.body:setPosition(boundary_right, y_self)
+  end
+
+  if input.up.active then
+    local boundary_top = math.max(vars.player_radius, y_self - vars.player_speed)
+    self.body:setPosition(x_self, boundary_top)
+  end
+
+  if input.down.active then
+    local boundary_down = math.min(vars.world_height - vars.player_radius, y_self + vars.player_speed)
+    self.body:setPosition(x_self, boundary_down)
+  end
+end
+
+entity.draw = function(self)
+  local x_self, y_self = self.body:getWorldCenter()
+  love.graphics.circle('fill', x_self, y_self, self.shape:getRadius())
+end
+
+return entity
--- dodge/vars.lua
+++ dodge/vars.lua
@@ -3,7 +3,10 @@ local vars = {
   gravity_multiplier = 128,
 
   world_width = 800,
-  world_height = 600
+  world_height = 600,
+
+  player_radius = 10,
+  player_speed = 5
 }
 
 return vars
