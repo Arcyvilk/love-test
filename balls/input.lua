acebc364c5d8d6e09d886a8c35d84a5165706c9b
Arcyvilk <arcyvilk@pm.me>
2026-02-04 14:50:15 +0100

    refactor: 💡 add movement input

--- balls/input.lua
+++ balls/input.lua
@@ -1,7 +1,49 @@
-local input = {}
+local input = {
+  escape = {
+    press = function(self)
+      love.event.quit()
+    end
+  },
 
-input.escape = function()
-  love.event.quit()
-end
+  left   = {
+    active = false,
+    press = function(self)
+      self.active = true
+    end,
+    release = function(self)
+      self.active = false
+    end
+  },
+
+  right  = {
+    active = false,
+    press = function(self)
+      self.active = true
+    end,
+    release = function(self)
+      self.active = false
+    end
+  },
+
+  up     = {
+    active = false,
+    press = function(self)
+      self.active = true
+    end,
+    release = function(self)
+      self.active = false
+    end
+  },
+
+  down   = {
+    active = false,
+    press = function(self)
+      self.active = true
+    end,
+    release = function(self)
+      self.active = false
+    end
+  }
+}
 
 return input
