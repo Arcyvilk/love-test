55c6f5e6d04dd1d477e97440150a55ad7a8149b1
Arcyvilk <arcyvilk@pm.me>
2026-02-05 22:49:26 +0100

    refactor: 💡 refactor player segments movement

--- dodge/entities/player.lua
+++ dodge/entities/player.lua
@@ -7,13 +7,12 @@ local segments       = {}
 
 --- @param index number
 local create_segment = function(index)
-  local segment = {
-    index = index
-  }
+  local segment = {}
+
+  local segment_radius = (vars.player_radius_max - vars.player_radius_min) / vars.player_segments
 
   segment.body = love.physics.newBody(world, vars.world_width / 2, vars.world_height / 2, 'kinematic')
-  segment.shape = love.physics.newCircleShape(clamp(vars.player_radius_min, vars.player_radius_max,
-    (vars.player_segments - index) / 2))
+  segment.shape = love.physics.newCircleShape(segment_radius * (vars.player_segments - index))
   segment.fixture = love.physics.newFixture(segment.body, segment.shape)
   segment.fixture:setUserData(segment)
   segment.target = {}
@@ -21,25 +20,22 @@ local create_segment = function(index)
   segment.update = function(self, delta)
     local px, py = self.body:getPosition()
     local is_head = index == 1
-    local distance_factor_x = 1
-    local distance_factor_y = 1
 
     if is_head then -- target cursor
       segment.target.x, segment.target.y = love.mouse.getPosition()
-    else            -- target previous chain segment
+      local vx, vy = normalize(segment.target.x - px, segment.target.y - py)
+      local distance_factor_x = math.abs(segment.target.x - px) / 100
+      local distance_factor_y = math.abs(segment.target.y - py) / 100
+      self.body:setLinearVelocity(
+        vx * clamp(vars.player_speed_min, vars.player_speed_max, vars.player_base_speed * distance_factor_x),
+        vy * clamp(vars.player_speed_min, vars.player_speed_max, vars.player_base_speed * distance_factor_y))
+    else -- target previous chain segment
       segment.target.x, segment.target.y = segments[index - 1].body:getPosition()
-    end
+      local vx, vy = normalize(px - segment.target.x, py - segment.target.y)
+      local spacing = segments[index - 1].shape:getRadius() * 2
+      local x = segment.target.x + vx * spacing
+      local y = segment.target.y + vy * spacing
 
-    local vx, vy = normalize(segment.target.x - px, segment.target.y - py)
-    distance_factor_x = math.abs(segment.target.x - px) / 100
-    distance_factor_y = math.abs(segment.target.y - py) / 100
-
-    if is_head then
-      self.body:setLinearVelocity(vx * vars.player_speed * distance_factor_x, vy * vars.player_speed * distance_factor_y)
-    else
-      local x = segment.target.x + (vx * segments[index - 1].shape:getRadius() * 2)
-      local y = segment.target.y + (vy * segments[index - 1].shape:getRadius() * 2)
-      if index == 2 then print(vx) end
       self.body:setPosition(x, y)
     end
   end
