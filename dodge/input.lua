db358c3563fbe9aa9358c4246cf4f9b9155511ab
Arcyvilk <arcyvilk@pm.me>
2026-02-05 11:38:52 +0100

    refactor: 💡 remove unnecessary input

--- dodge/input.lua
+++ dodge/input.lua
@@ -1,47 +1,7 @@
 local input = {
   escape = {
-    press = function(self)
+    press = function()
       love.event.quit()
-    end
-  },
-
-  left   = {
-    active = false,
-    press = function(self)
-      self.active = true
-    end,
-    release = function(self)
-      self.active = false
-    end
-  },
-
-  right  = {
-    active = false,
-    press = function(self)
-      self.active = true
-    end,
-    release = function(self)
-      self.active = false
-    end
-  },
-
-  up     = {
-    active = false,
-    press = function(self)
-      self.active = true
-    end,
-    release = function(self)
-      self.active = false
-    end
-  },
-
-  down   = {
-    active = false,
-    press = function(self)
-      self.active = true
-    end,
-    release = function(self)
-      self.active = false
     end
   }
 }
