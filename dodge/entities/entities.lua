dc934107622153b605d49a90b0b4e9f8f87ecc67
Arcyvilk <arcyvilk@pm.me>
2026-02-05 22:48:49 +0100

    refactor: 💡 draw a spawner entity

---
+++ dodge/entities/entities.lua
@@ -0,0 +1,17 @@
+local vars          = require 'vars'
+local spawner       = require 'entities.spawner'
+
+local spawners      = {
+  spawner(vars.spawner_offset_x, vars.spawner_offset_y, 1),
+  spawner(vars.world_width - vars.spawner_offset_x, vars.spawner_offset_y, 1)
+}
+
+local draw_spawners = function()
+  for _, entity in ipairs(spawners) do
+    entity:draw()
+  end
+end
+
+return {
+  draw_spawners
+}
--- dodge/main.lua
+++ dodge/main.lua
@@ -1,23 +1,25 @@
-local world      = require 'world'
-local input      = require 'input'
-local player     = require 'entities.player'
+local world         = require 'world'
+local input         = require 'input'
+local player        = require 'entities.player'
+local draw_spawners = unpack(require 'entities.entities')
 
-love.draw        = function()
+love.draw           = function()
   player:draw()
+  draw_spawners()
 end
 
-love.update      = function(dt)
-  player:update(dt)
+love.update         = function(dt)
+  player:update()
   world:update(dt)
 end
 
-love.keypressed  = function(pressed_key)
+love.keypressed     = function(pressed_key)
   if input[pressed_key] then
     input[pressed_key]:press()
   end
 end
 
-love.keyreleased = function(released_key)
+love.keyreleased    = function(released_key)
   if input[released_key] then
     input[released_key]:release()
   end
