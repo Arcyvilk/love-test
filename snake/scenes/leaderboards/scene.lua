2ebae530ab3abb54344b0bd97de300835bbe31c7
Arcyvilk <arcyvilk@pm.me>
2026-02-12 19:33:17 +0100

    refactor: 💡 add functional leaderboards

--- snake/scenes/leaderboards/scene.lua
+++ snake/scenes/leaderboards/scene.lua
@@ -4,9 +4,33 @@ local input         = require 'scenes.end.input'
 local center_text   = require 'utils.center_text'
 local vars          = require 'vars'
 
+local create_entry  = function(text, offset_y)
+  return {
+    text = text,
+    size = 12,
+    color = { 1, 1, 1, 1 },
+    offset_x = 0,
+    offset_y = offset_y,
+    parent = { x = 0, y = 0, width = vars.world_width, height = vars.world_height }
+  }
+end
+
 return function(scene_manager)
   local buttons = {
     create_button("Back", 0, vars.world_height / 2.4, 160, 40, function() scene_manager:move_to('scene_home') end)
+  }
+
+  local entries = {
+    create_entry("1. not you", -100),
+    create_entry("2. not you", -80),
+    create_entry("3. not you", -60),
+    create_entry("4. not you", -40),
+    create_entry("5. not you", -20),
+    create_entry("6. not you", 0),
+    create_entry("7. not you", 20),
+    create_entry("8. not you", 40),
+    create_entry("9. not you", 60),
+    create_entry("10. not you", 80)
   }
 
   local scene = create_scene({
@@ -32,6 +56,9 @@ return function(scene_manager)
 
       for _, button in ipairs(buttons) do
         button:draw()
+      end
+      for _, entry in ipairs(entries) do
+        center_text(entry)
       end
     end,
 
