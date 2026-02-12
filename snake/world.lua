73bf39793ce4869bd60fa7852a9da140ca142c18
Arcyvilk <arcyvilk@pm.me>
2026-02-12 20:49:05 +0100

    refactor: 💡 add begin_collission to world physics

--- snake/world.lua
+++ snake/world.lua
@@ -1,19 +1,27 @@
 local vars                   = require 'vars'
 
+local get_entity_by_name     = function(entity_name, entity_a, entity_b)
+  if entity_a and entity_a.name == entity_name then return entity_a end
+  if entity_b and entity_b.name == entity_name then return entity_b end
+end
+
 local begin_contact_callback = function(fixture_a, fixture_b, contact)
-  return
+  local entity_a = fixture_a:getUserData()
+  local entity_b = fixture_b:getUserData()
+
+  local head = get_entity_by_name('player_head', entity_a, entity_b)
+  local arrow = get_entity_by_name('arrow', entity_a, entity_b)
+
+  if head and arrow then head:take_damage() end
 end
 
 local end_contact_callback   = function(fixture_a, fixture_b, contact)
-  return
 end
 
 local pre_solve_callback     = function(fixture_a, fixture_b, contact)
-  return
 end
 
 local post_solve_callback    = function(fixture_a, fixture_b, contact)
-  return
 end
 
 local world                  = love.physics.newWorld(0, vars.gravity * vars.gravity_multiplier)
