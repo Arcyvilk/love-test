a1baee5ab9c125c43c96310e8b7999685b7603ed
Arcyvilk <arcyvilk@pm.me>
2026-02-13 01:53:24 +0100

    refactor: 💡 update global vars

--- snake/vars.lua
+++ snake/vars.lua
@@ -9,7 +9,7 @@ local vars = {
   player_radius_max = 25,
   player_base_speed = 500,
   player_segments = 10,
-  player_recovery_time = 1, -- seconds
+  player_recovery_time = 0.75, -- seconds
 
   spawner_offset_x = 20,
   spawner_offset_y = 20,
@@ -17,7 +17,11 @@ local vars = {
   arrow_height = 50,
   arrow_width = 1,
   arrow_base_speed = 1000,
-  arrow_spawn_delay = 0.5
+  arrow_spawn_delay = 0.5,
+  arrow_preparation_time = 60,
+  arrow_ttl = 180,
+
+  save_file_name = "save_data.txt"
 }
 
 return vars
